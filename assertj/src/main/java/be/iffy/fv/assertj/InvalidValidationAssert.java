2c04bfe1ebedb6ae4e95a626b2a382d214c6d11e
Christophe Vanfleteren <c@vanfleteren.net>
2026-05-24 18:07:10 +0200

    fix assert

--- assertj/src/main/java/be/iffy/fv/assertj/InvalidValidationAssert.java
+++ assertj/src/main/java/be/iffy/fv/assertj/InvalidValidationAssert.java
@@ -2,6 +2,7 @@ package be.iffy.fv.assertj;
 
 import be.iffy.fv.ErrorMessage;
 import be.iffy.fv.Validation;
+import io.vavr.collection.HashMap;
 import io.vavr.collection.Map;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.Assertions;
@@ -40,13 +41,27 @@ public class InvalidValidationAssert<SELF extends InvalidValidationAssert<SELF, 
     /**
      * Asserts that the validation contains an error with the specified key and arguments.
      *
-     * @param errorKey the error key.
+     * @param errorMessage the error message.
      * @param args     the error arguments.
      * @return {@code this} assertion object.
      */
-    public SELF hasErrorMessage(String errorKey, Map<String, Object> args) {
-        assertThat(actual.errors()).map(ErrorMessage::key).contains(errorKey);
-        assertThat(actual.errors().filter(e -> e.key().equals(errorKey)).head().parameters()).isEqualTo(args);
+    public SELF hasErrorMessage(String errorMessage, Map<String, Object> args) {
+        assertThat(actual.errors()).map(ErrorMessage::message).contains(errorMessage);
+        assertThat(actual.errors().filter(e -> e.message().equals(errorMessage)).head().parameters()).isEqualTo(args);
+        return (SELF) this;
+    }
+
+    /**
+     * Asserts that the validation contains an error with the specified key and arguments.
+     *
+     * @param errorMessage the error message.
+     * @param args     the error arguments.
+     * @return {@code this} assertion object.
+     */
+    public SELF hasErrorMessage(String errorMessage, java.util.Map<String, Object> args) {
+        Map<String,Object> map = HashMap.ofAll(args);
+        assertThat(actual.errors()).map(ErrorMessage::message).contains(errorMessage);
+        assertThat(actual.errors().filter(e -> e.message().equals(errorMessage)).head().parameters()).isEqualTo(map);
         return (SELF) this;
     }
 
