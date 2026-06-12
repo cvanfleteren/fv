8cbc289e3e0a31a6aac135e748baff261116f0a1
Christophe Vanfleteren <c@vanfleteren.net>
2026-06-12 23:47:04 +0200

    move combine to MappingRules

--- core/src/main/java/be/iffy/fv/MappingRule.java
+++ core/src/main/java/be/iffy/fv/MappingRule.java
@@ -1,7 +1,8 @@
 package be.iffy.fv;
 
 import be.iffy.fv.Validation.Invalid;
-import io.vavr.*;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import io.vavr.collection.HashMap;
 import io.vavr.collection.List;
 import io.vavr.collection.Map;
@@ -423,94 +424,4 @@ public interface MappingRule<T, R> extends Function<T, Validation<R>> {
         );
     }
 
-    /**
-     * Combines two mapping rules into a builder that can map all valid values or accumulate all errors.
-     */
-    static <T, R1, R2> CombineBuilder2<T, R1, R2> combine(MappingRule<T, R1> r1, MappingRule<T, R2> r2) {
-        return new CombineBuilder2<>(r1, r2);
-    }
-
-    /**
-     * Combines three mapping rules into a builder that can map all valid values or accumulate all errors.
-     */
-    static <T, R1, R2, R3> CombineBuilder3<T, R1, R2, R3> combine(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3) {
-        return new CombineBuilder3<>(r1, r2, r3);
-    }
-
-    /**
-     * Combines four mapping rules into a builder that can map all valid values or accumulate all errors.
-     */
-    static <T, R1, R2, R3, R4> CombineBuilder4<T, R1, R2, R3, R4> combine(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4) {
-        return new CombineBuilder4<>(r1, r2, r3, r4);
-    }
-
-    /**
-     * Combines five mapping rules into a builder that can map all valid values or accumulate all errors.
-     */
-    static <T, R1, R2, R3, R4, R5> CombineBuilder5<T, R1, R2, R3, R4, R5> combine(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4, MappingRule<T, R5> r5) {
-        return new CombineBuilder5<>(r1, r2, r3, r4, r5);
-    }
-
-    /**
-     * Combines six mapping rules into a builder that can map all valid values or accumulate all errors.
-     */
-    static <T, R1, R2, R3, R4, R5, R6> CombineBuilder6<T, R1, R2, R3, R4, R5, R6> combine(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4, MappingRule<T, R5> r5, MappingRule<T, R6> r6) {
-        return new CombineBuilder6<>(r1, r2, r3, r4, r5, r6);
-    }
-
-    /**
-     * Combines seven mapping rules into a builder that can map all valid values or accumulate all errors.
-     */
-    static <T, R1, R2, R3, R4, R5, R6, R7> CombineBuilder7<T, R1, R2, R3, R4, R5, R6, R7> combine(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4, MappingRule<T, R5> r5, MappingRule<T, R6> r6, MappingRule<T, R7> r7) {
-        return new CombineBuilder7<>(r1, r2, r3, r4, r5, r6, r7);
-    }
-
-    /**
-     * Combines eight mapping rules into a builder that can map all valid values or accumulate all errors.
-     */
-    static <T, R1, R2, R3, R4, R5, R6, R7, R8> CombineBuilder8<T, R1, R2, R3, R4, R5, R6, R7, R8> combine(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4, MappingRule<T, R5> r5, MappingRule<T, R6> r6, MappingRule<T, R7> r7, MappingRule<T, R8> r8) {
-        return new CombineBuilder8<>(r1, r2, r3, r4, r5, r6, r7, r8);
-    }
-
-    record CombineBuilder2<T, R1, R2>(MappingRule<T, R1> r1, MappingRule<T, R2> r2) {
-        public <R> MappingRule<T, R> map(Function2<? super R1, ? super R2, ? extends R> mapper) {
-            return input -> Validations.combine(r1.test(input), r2.test(input)).map(mapper);
-        }
-    }
-
-    record CombineBuilder3<T, R1, R2, R3>(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3) {
-        public <R> MappingRule<T, R> map(Function3<? super R1, ? super R2, ? super R3, ? extends R> mapper) {
-            return input -> Validations.combine(r1.test(input), r2.test(input), r3.test(input)).map(mapper);
-        }
-    }
-
-    record CombineBuilder4<T, R1, R2, R3, R4>(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4) {
-        public <R> MappingRule<T, R> map(Function4<? super R1, ? super R2, ? super R3, ? super R4, ? extends R> mapper) {
-            return input -> Validations.combine(r1.test(input), r2.test(input), r3.test(input), r4.test(input)).map(mapper);
-        }
-    }
-
-    record CombineBuilder5<T, R1, R2, R3, R4, R5>(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4, MappingRule<T, R5> r5) {
-        public <R> MappingRule<T, R> map(Function5<? super R1, ? super R2, ? super R3, ? super R4, ? super R5, ? extends R> mapper) {
-            return input -> Validations.combine(r1.test(input), r2.test(input), r3.test(input), r4.test(input), r5.test(input)).map(mapper);
-        }
-    }
-
-    record CombineBuilder6<T, R1, R2, R3, R4, R5, R6>(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4, MappingRule<T, R5> r5, MappingRule<T, R6> r6) {
-        public <R> MappingRule<T, R> map(Function6<? super R1, ? super R2, ? super R3, ? super R4, ? super R5, ? super R6, ? extends R> mapper) {
-            return input -> Validations.combine(r1.test(input), r2.test(input), r3.test(input), r4.test(input), r5.test(input), r6.test(input)).map(mapper);
-        }
-    }
-
-    record CombineBuilder7<T, R1, R2, R3, R4, R5, R6, R7>(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4, MappingRule<T, R5> r5, MappingRule<T, R6> r6, MappingRule<T, R7> r7) {
-        public <R> MappingRule<T, R> map(Function7<? super R1, ? super R2, ? super R3, ? super R4, ? super R5, ? super R6, ? super R7, ? extends R> mapper) {
-            return input -> Validations.combine(r1.test(input), r2.test(input), r3.test(input), r4.test(input), r5.test(input), r6.test(input), r7.test(input)).map(mapper);
-        }
-    }
-
-    record CombineBuilder8<T, R1, R2, R3, R4, R5, R6, R7, R8>(MappingRule<T, R1> r1, MappingRule<T, R2> r2, MappingRule<T, R3> r3, MappingRule<T, R4> r4, MappingRule<T, R5> r5, MappingRule<T, R6> r6, MappingRule<T, R7> r7, MappingRule<T, R8> r8) {
-        public <R> MappingRule<T, R> map(Function8<? super R1, ? super R2, ? super R3, ? super R4, ? super R5, ? super R6, ? super R7, ? super R8, ? extends R> mapper) {
-            return input -> Validations.combine(r1.test(input), r2.test(input), r3.test(input), r4.test(input), r5.test(input), r6.test(input), r7.test(input), r8.test(input)).map(mapper);
-        }
-    }
 }
---
+++ core/src/test/java/be/iffy/fv/MappingRulesTest.java
@@ -0,0 +1,162 @@
+package be.iffy.fv;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static be.iffy.fv.assertj.ValidationAssert.assertThatValidation;
+
+class MappingRulesTest {
+
+    @Nested
+    class Combine2 {
+
+        @Test
+        void combine_whenBothRulesAreValid_returnsCombinedResult() {
+            MappingRule<String, Integer> rule1 = s -> Validation.valid(s.length());
+            MappingRule<String, String> rule2 = s -> Validation.valid(s.toUpperCase());
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2).map((length, upper) -> length + ":" + upper);
+
+            assertThatValidation(combined.test("hello"))
+                    .isValid()
+                    .isEqualTo("5:HELLO");
+        }
+
+        @Test
+        void combine_whenBothRulesAreInvalid_returnsCombinedErrors() {
+            MappingRule<String, Integer> rule1 = s -> Validation.invalid("error.one");
+            MappingRule<String, String> rule2 = s -> Validation.invalid("error.two");
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2).map((length, upper) -> length + ":" + upper);
+
+            assertThatValidation(combined.test("hello"))
+                    .isInvalid()
+                    .hasErrorMessage("error.one")
+                    .hasErrorMessage("error.two");
+        }
+
+        @Test
+        void combine_whenOneRuleIsInvalid_returnsInvalidWithCombinedErrors() {
+            MappingRule<String, Integer> rule1 = s -> Validation.valid(s.length());
+            MappingRule<String, String> rule2 = s -> Validation.invalid("error.two");
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2).map((length, upper) -> length + ":" + upper);
+
+            assertThatValidation(combined.test("hello"))
+                    .isInvalid()
+                    .hasErrorMessage("error.two");
+        }
+    }
+
+    @Nested
+    class Combine3 {
+        @Test
+        void combine_whenAllRulesAreValid_returnsCombinedResult() {
+            MappingRule<String, Integer> rule1 = s -> Validation.valid(s.length());
+            MappingRule<String, String> rule2 = s -> Validation.valid(s.toUpperCase());
+            MappingRule<String, String> rule3 = s -> Validation.valid("static");
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2, rule3).map((l, u, s) -> l + ":" + u + ":" + s);
+
+            assertThatValidation(combined.test("hello"))
+                    .isValid()
+                    .isEqualTo("5:HELLO:static");
+        }
+    }
+
+    @Nested
+    class Combine4 {
+        @Test
+        void combine_whenAllRulesAreValid_returnsCombinedResult() {
+            MappingRule<String, Integer> rule1 = s -> Validation.valid(s.length());
+            MappingRule<String, String> rule2 = s -> Validation.valid(s.toUpperCase());
+            MappingRule<String, String> rule3 = s -> Validation.valid("a");
+            MappingRule<String, String> rule4 = s -> Validation.valid("b");
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2, rule3, rule4).map((l, u, a, b) -> l + ":" + u + ":" + a + ":" + b);
+
+            assertThatValidation(combined.test("hello"))
+                    .isValid()
+                    .isEqualTo("5:HELLO:a:b");
+        }
+    }
+
+    @Nested
+    class Combine5 {
+        @Test
+        void combine_whenAllRulesAreValid_returnsCombinedResult() {
+            MappingRule<String, Integer> rule1 = s -> Validation.valid(s.length());
+            MappingRule<String, String> rule2 = s -> Validation.valid(s.toUpperCase());
+            MappingRule<String, String> rule3 = s -> Validation.valid("a");
+            MappingRule<String, String> rule4 = s -> Validation.valid("b");
+            MappingRule<String, String> rule5 = s -> Validation.valid("c");
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2, rule3, rule4, rule5).map((l, u, a, b, c) -> l + ":" + u + ":" + a + ":" + b + ":" + c);
+
+            assertThatValidation(combined.test("hello"))
+                    .isValid()
+                    .isEqualTo("5:HELLO:a:b:c");
+        }
+    }
+
+    @Nested
+    class Combine6 {
+        @Test
+        void combine_whenAllRulesAreValid_returnsCombinedResult() {
+            MappingRule<String, Integer> rule1 = s -> Validation.valid(s.length());
+            MappingRule<String, String> rule2 = s -> Validation.valid(s.toUpperCase());
+            MappingRule<String, String> rule3 = s -> Validation.valid("a");
+            MappingRule<String, String> rule4 = s -> Validation.valid("b");
+            MappingRule<String, String> rule5 = s -> Validation.valid("c");
+            MappingRule<String, String> rule6 = s -> Validation.valid("d");
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2, rule3, rule4, rule5, rule6).map((l, u, a, b, c, d) -> l + ":" + u + ":" + a + ":" + b + ":" + c + ":" + d);
+
+            assertThatValidation(combined.test("hello"))
+                    .isValid()
+                    .isEqualTo("5:HELLO:a:b:c:d");
+        }
+    }
+
+    @Nested
+    class Combine7 {
+        @Test
+        void combine_whenAllRulesAreValid_returnsCombinedResult() {
+            MappingRule<String, Integer> rule1 = s -> Validation.valid(s.length());
+            MappingRule<String, String> rule2 = s -> Validation.valid(s.toUpperCase());
+            MappingRule<String, String> rule3 = s -> Validation.valid("a");
+            MappingRule<String, String> rule4 = s -> Validation.valid("b");
+            MappingRule<String, String> rule5 = s -> Validation.valid("c");
+            MappingRule<String, String> rule6 = s -> Validation.valid("d");
+            MappingRule<String, String> rule7 = s -> Validation.valid("e");
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2, rule3, rule4, rule5, rule6, rule7).map((l, u, a, b, c, d, e) -> l + ":" + u + ":" + a + ":" + b + ":" + c + ":" + d + ":" + e);
+
+            assertThatValidation(combined.test("hello"))
+                    .isValid()
+                    .isEqualTo("5:HELLO:a:b:c:d:e");
+        }
+    }
+
+    @Nested
+    class Combine8 {
+        @Test
+        void combine_whenAllRulesAreValid_returnsCombinedResult() {
+            MappingRule<String, Integer> rule1 = s -> Validation.valid(s.length());
+            MappingRule<String, String> rule2 = s -> Validation.valid(s.toUpperCase());
+            MappingRule<String, String> rule3 = s -> Validation.valid("a");
+            MappingRule<String, String> rule4 = s -> Validation.valid("b");
+            MappingRule<String, String> rule5 = s -> Validation.valid("c");
+            MappingRule<String, String> rule6 = s -> Validation.valid("d");
+            MappingRule<String, String> rule7 = s -> Validation.valid("e");
+            MappingRule<String, String> rule8 = s -> Validation.valid("f");
+
+            MappingRule<String, String> combined = MappingRules.combine(rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8).map((l, u, a, b, c, d, e, f) -> l + ":" + u + ":" + a + ":" + b + ":" + c + ":" + d + ":" + e + ":" + f);
+
+            assertThatValidation(combined.test("hello"))
+                    .isValid()
+                    .isEqualTo("5:HELLO:a:b:c:d:e:f");
+        }
+    }
+
+}
