6c22425ceb8b9fd4ee79be6ac5d715112ae829b4
Christophe Vanfleteren <c@vanfleteren.net>
2026-06-17 02:26:28 +0200

    drop Tuples in ValidatingBuilders

--- dsl/src/main/java/be/iffy/fv/dsl/impl/ValidatingDSL.java
+++ dsl/src/main/java/be/iffy/fv/dsl/impl/ValidatingDSL.java
@@ -16,10 +16,12 @@ public final class ValidatingDSL {
      * Combine multiple Validations, allowing you to map or flatMap over their values.
      */
     public static class ValidatingBuilder2<T1, T2> {
-        private final Tuple2<Validation<T1>, Validation<T2>> vs;
+        private final Validation<T1> v1;
+        private final Validation<T2> v2;
 
         public ValidatingBuilder2(Validation<T1> v1, Validation<T2> v2) {
-            this.vs = Tuple.of(Objects.requireNonNull(v1), Objects.requireNonNull(v2));
+            this.v1 = Objects.requireNonNull(v1);
+            this.v2 = Objects.requireNonNull(v2);
         }
 
         /**
@@ -27,7 +29,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> map(Function2<T1, T2, T> mapper) {
-            return Validations.combine(vs._1, vs._2).map(mapper);
+            return Validations.combine(v1, v2).map(mapper);
         }
 
         /**
@@ -35,7 +37,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> flatMap(Function2<T1, T2, Validation<? extends T>> mapper) {
-            return Validations.combine(vs._1, vs._2).flatMap(mapper);
+            return Validations.combine(v1, v2).flatMap(mapper);
         }
     }
 
@@ -43,10 +45,14 @@ public final class ValidatingDSL {
      * Combine multiple Validations, allowing you to map or flatMap over their values.
      */
     public static class ValidatingBuilder3<T1, T2, T3> {
-        private final Tuple3<Validation<T1>, Validation<T2>, Validation<T3>> vs;
+        private final Validation<T1> v1;
+        private final Validation<T2> v2;
+        private final Validation<T3> v3;
 
         public ValidatingBuilder3(Validation<T1> v1, Validation<T2> v2, Validation<T3> v3) {
-            this.vs = Tuple.of(Objects.requireNonNull(v1), Objects.requireNonNull(v2), Objects.requireNonNull(v3));
+            this.v1 = Objects.requireNonNull(v1);
+            this.v2 = Objects.requireNonNull(v2);
+            this.v3 = Objects.requireNonNull(v3);
         }
 
         /**
@@ -54,7 +60,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> map(Function3<T1, T2, T3, T> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3).map(mapper);
+            return Validations.combine(v1, v2, v3).map(mapper);
         }
 
         /**
@@ -62,7 +68,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> flatMap(Function3<T1, T2, T3, Validation<? extends T>> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3).flatMap(mapper);
+            return Validations.combine(v1, v2, v3).flatMap(mapper);
         }
     }
 
@@ -70,15 +76,16 @@ public final class ValidatingDSL {
      * Combine multiple Validations, allowing you to map or flatMap over their values.
      */
     public static class ValidatingBuilder4<T1, T2, T3, T4> {
-        private final Tuple4<Validation<T1>, Validation<T2>, Validation<T3>, Validation<T4>> vs;
+        private final Validation<T1> v1;
+        private final Validation<T2> v2;
+        private final Validation<T3> v3;
+        private final Validation<T4> v4;
 
         public ValidatingBuilder4(Validation<T1> v1, Validation<T2> v2, Validation<T3> v3, Validation<T4> v4) {
-            this.vs = Tuple.of(
-                Objects.requireNonNull(v1),
-                Objects.requireNonNull(v2),
-                Objects.requireNonNull(v3),
-                Objects.requireNonNull(v4)
-            );
+            this.v1 = Objects.requireNonNull(v1);
+            this.v2 = Objects.requireNonNull(v2);
+            this.v3 = Objects.requireNonNull(v3);
+            this.v4 = Objects.requireNonNull(v4);
         }
 
         /**
@@ -86,7 +93,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> map(Function4<T1, T2, T3, T4, T> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4).map(mapper);
+            return Validations.combine(v1, v2, v3, v4).map(mapper);
         }
 
         /**
@@ -94,7 +101,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> flatMap(Function4<T1, T2, T3, T4, Validation<? extends T>> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4).flatMap(mapper);
+            return Validations.combine(v1, v2, v3, v4).flatMap(mapper);
         }
     }
 
@@ -102,16 +109,18 @@ public final class ValidatingDSL {
      * Combine multiple Validations, allowing you to map or flatMap over their values.
      */
     public static class ValidatingBuilder5<T1, T2, T3, T4, T5> {
-        private final Tuple5<Validation<T1>, Validation<T2>, Validation<T3>, Validation<T4>, Validation<T5>> vs;
+        private final Validation<T1> v1;
+        private final Validation<T2> v2;
+        private final Validation<T3> v3;
+        private final Validation<T4> v4;
+        private final Validation<T5> v5;
 
         public ValidatingBuilder5(Validation<T1> v1, Validation<T2> v2, Validation<T3> v3, Validation<T4> v4, Validation<T5> v5) {
-            this.vs = Tuple.of(
-                Objects.requireNonNull(v1),
-                Objects.requireNonNull(v2),
-                Objects.requireNonNull(v3),
-                Objects.requireNonNull(v4),
-                Objects.requireNonNull(v5)
-            );
+            this.v1 = Objects.requireNonNull(v1);
+            this.v2 = Objects.requireNonNull(v2);
+            this.v3 = Objects.requireNonNull(v3);
+            this.v4 = Objects.requireNonNull(v4);
+            this.v5 = Objects.requireNonNull(v5);
         }
 
         /**
@@ -119,7 +128,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> map(Function5<T1, T2, T3, T4, T5, T> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4, vs._5).map(mapper);
+            return Validations.combine(v1, v2, v3, v4, v5).map(mapper);
         }
 
         /**
@@ -127,7 +136,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> flatMap(Function5<T1, T2, T3, T4, T5, Validation<? extends T>> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4, vs._5).flatMap(mapper);
+            return Validations.combine(v1, v2, v3, v4, v5).flatMap(mapper);
         }
     }
 
@@ -135,17 +144,20 @@ public final class ValidatingDSL {
      * Combine multiple Validations, allowing you to map or flatMap over their values.
      */
     public static class ValidatingBuilder6<T1, T2, T3, T4, T5, T6> {
-        private final Tuple6<Validation<T1>, Validation<T2>, Validation<T3>, Validation<T4>, Validation<T5>, Validation<T6>> vs;
+        private final Validation<T1> v1;
+        private final Validation<T2> v2;
+        private final Validation<T3> v3;
+        private final Validation<T4> v4;
+        private final Validation<T5> v5;
+        private final Validation<T6> v6;
 
         public ValidatingBuilder6(Validation<T1> v1, Validation<T2> v2, Validation<T3> v3, Validation<T4> v4, Validation<T5> v5, Validation<T6> v6) {
-            this.vs = Tuple.of(
-                Objects.requireNonNull(v1),
-                Objects.requireNonNull(v2),
-                Objects.requireNonNull(v3),
-                Objects.requireNonNull(v4),
-                Objects.requireNonNull(v5),
-                Objects.requireNonNull(v6)
-            );
+            this.v1 = Objects.requireNonNull(v1);
+            this.v2 = Objects.requireNonNull(v2);
+            this.v3 = Objects.requireNonNull(v3);
+            this.v4 = Objects.requireNonNull(v4);
+            this.v5 = Objects.requireNonNull(v5);
+            this.v6 = Objects.requireNonNull(v6);
         }
 
         /**
@@ -153,7 +165,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> map(Function6<T1, T2, T3, T4, T5, T6, T> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4, vs._5, vs._6).map(mapper);
+            return Validations.combine(v1, v2, v3, v4, v5, v6).map(mapper);
         }
 
         /**
@@ -161,7 +173,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> flatMap(Function6<T1, T2, T3, T4, T5, T6, Validation<? extends T>> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4, vs._5, vs._6).flatMap(mapper);
+            return Validations.combine(v1, v2, v3, v4, v5, v6).flatMap(mapper);
         }
     }
 
@@ -169,18 +181,22 @@ public final class ValidatingDSL {
      * Combine multiple Validations, allowing you to map or flatMap over their values.
      */
     public static class ValidatingBuilder7<T1, T2, T3, T4, T5, T6, T7> {
-        private final Tuple7<Validation<T1>, Validation<T2>, Validation<T3>, Validation<T4>, Validation<T5>, Validation<T6>, Validation<T7>> vs;
+        private final Validation<T1> v1;
+        private final Validation<T2> v2;
+        private final Validation<T3> v3;
+        private final Validation<T4> v4;
+        private final Validation<T5> v5;
+        private final Validation<T6> v6;
+        private final Validation<T7> v7;
 
         public ValidatingBuilder7(Validation<T1> v1, Validation<T2> v2, Validation<T3> v3, Validation<T4> v4, Validation<T5> v5, Validation<T6> v6, Validation<T7> v7) {
-            this.vs = Tuple.of(
-                Objects.requireNonNull(v1),
-                Objects.requireNonNull(v2),
-                Objects.requireNonNull(v3),
-                Objects.requireNonNull(v4),
-                Objects.requireNonNull(v5),
-                Objects.requireNonNull(v6),
-                Objects.requireNonNull(v7)
-            );
+            this.v1 = Objects.requireNonNull(v1);
+            this.v2 = Objects.requireNonNull(v2);
+            this.v3 = Objects.requireNonNull(v3);
+            this.v4 = Objects.requireNonNull(v4);
+            this.v5 = Objects.requireNonNull(v5);
+            this.v6 = Objects.requireNonNull(v6);
+            this.v7 = Objects.requireNonNull(v7);
         }
 
         /**
@@ -188,7 +204,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> map(Function7<T1, T2, T3, T4, T5, T6, T7, T> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4, vs._5, vs._6, vs._7).map(mapper);
+            return Validations.combine(v1, v2, v3, v4, v5, v6, v7).map(mapper);
         }
 
         /**
@@ -196,7 +212,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> flatMap(Function7<T1, T2, T3, T4, T5, T6, T7, Validation<? extends T>> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4, vs._5, vs._6, vs._7).flatMap(mapper);
+            return Validations.combine(v1, v2, v3, v4, v5, v6, v7).flatMap(mapper);
         }
     }
 
@@ -204,19 +220,24 @@ public final class ValidatingDSL {
      * Combine multiple Validations, allowing you to map or flatMap over their values.
      */
     public static class ValidatingBuilder8<T1, T2, T3, T4, T5, T6, T7, T8> {
-        private final Tuple8<Validation<T1>, Validation<T2>, Validation<T3>, Validation<T4>, Validation<T5>, Validation<T6>, Validation<T7>, Validation<T8>> vs;
+        private final Validation<T1> v1;
+        private final Validation<T2> v2;
+        private final Validation<T3> v3;
+        private final Validation<T4> v4;
+        private final Validation<T5> v5;
+        private final Validation<T6> v6;
+        private final Validation<T7> v7;
+        private final Validation<T8> v8;
 
        public ValidatingBuilder8(Validation<T1> v1, Validation<T2> v2, Validation<T3> v3, Validation<T4> v4, Validation<T5> v5, Validation<T6> v6, Validation<T7> v7, Validation<T8> v8) {
-            this.vs = Tuple.of(
-                Objects.requireNonNull(v1),
-                Objects.requireNonNull(v2),
-                Objects.requireNonNull(v3),
-                Objects.requireNonNull(v4),
-                Objects.requireNonNull(v5),
-                Objects.requireNonNull(v6),
-                Objects.requireNonNull(v7),
-                Objects.requireNonNull(v8)
-            );
+            this.v1 = Objects.requireNonNull(v1);
+            this.v2 = Objects.requireNonNull(v2);
+            this.v3 = Objects.requireNonNull(v3);
+            this.v4 = Objects.requireNonNull(v4);
+            this.v5 = Objects.requireNonNull(v5);
+            this.v6 = Objects.requireNonNull(v6);
+            this.v7 = Objects.requireNonNull(v7);
+            this.v8 = Objects.requireNonNull(v8);
         }
 
         /**
@@ -224,7 +245,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> map(Function8<T1, T2, T3, T4, T5, T6, T7, T8, T> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4, vs._5, vs._6, vs._7, vs._8).map(mapper);
+            return Validations.combine(v1, v2, v3, v4, v5, v6, v7, v8).map(mapper);
         }
 
         /**
@@ -232,7 +253,7 @@ public final class ValidatingDSL {
          */
         @Contract(pure = true)
         public <T> Validation<T> flatMap(Function8<T1, T2, T3, T4, T5, T6, T7, T8, Validation<? extends T>> mapper) {
-            return Validations.combine(vs._1, vs._2, vs._3, vs._4, vs._5, vs._6, vs._7, vs._8).flatMap(mapper);
+            return Validations.combine(v1, v2, v3, v4, v5, v6, v7, v8).flatMap(mapper);
         }
     }
 
