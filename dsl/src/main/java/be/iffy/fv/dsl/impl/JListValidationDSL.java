9e31a1f522dca50cae2f5871c4b58a0bcf8b835d
Christophe Vanfleteren <c@vanfleteren.net>
2026-06-18 23:56:21 +0200

    improve implementation and javadoc for validateThatList

--- dsl/src/main/java/be/iffy/fv/dsl/impl/JListValidationDSL.java
+++ dsl/src/main/java/be/iffy/fv/dsl/impl/JListValidationDSL.java
@@ -4,11 +4,13 @@ import be.iffy.fv.MappingRule;
 import be.iffy.fv.Rule;
 import be.iffy.fv.Validation;
 import be.iffy.fv.Validations;
+import io.vavr.control.Option;
 import org.jetbrains.annotations.Contract;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * DSL class for validating a vavr List, allowing you to easily express rules on the list itself
@@ -17,32 +19,40 @@ import java.util.function.Function;
 public final class JListValidationDSL<L, E> {
     private final Validation<List<L>> listValidation;
     private final Validation<List<E>> elementValidation;
-    private final String name;
+    private final Option<String> name;
 
+    @SuppressWarnings("unchecked")
     public JListValidationDSL(List<L> value, String name) {
         this(
-                Rule.<List<L>>notNull().apply(value),
-                //E and L start out the same
-                Rule.<List<E>>notNull().apply((List<E>) value),
-                name
+            Validation.fromNullable(value),
+            //E and L start out the same
+            Validation.fromNullable((List<E>) value),
+            Option.of(name)
         );
     }
 
-    private JListValidationDSL(Validation<List<L>> listValidation, Validation<List<E>> elementValidation, String name) {
+    private JListValidationDSL(Validation<List<L>> listValidation, Validation<List<E>> elementValidation, Option<String> name) {
         this.listValidation = Objects.requireNonNull(listValidation, "validation cannot be null");
         this.elementValidation = Objects.requireNonNull(elementValidation, "validation cannot be null");
-        this.name = name;
+        this.name = name.filter(Predicate.not(String::isBlank));
     }
 
+    /**
+     * Completes the validation process and returns the final {@link Validation} result.
+     * This combines both the list-level validations and the element-level validations.
+     */
     @Contract(pure = true)
     public Validation<List<E>> validate() {
         return Validations
                 .combine(
-                    listValidation.at(name),
-                    elementValidation.at(name)
+                    validationAtName(listValidation),
+                    validationAtName(elementValidation)
                 ).map((list, elements) -> elements);
     }
 
+    /**
+     * Applies the given validation rule to each element in the list.
+     */
     @Contract(pure = true)
     public <R> JListValidationDSL<R, R> eachIs(Function<E, Validation<R>> rule) {
         Validation<List<R>> newElements = elementValidation.refine(list -> MappingRule.of(rule).lift().toList().apply(list));
@@ -61,5 +71,13 @@ public final class JListValidationDSL<L, E> {
     public JListValidationDSL<L, E> is(Rule<List<L>> rule) {
         Validation<List<L>> ruleValidation = Validation.narrowSuper(listValidation.refine(rule));
         return new JListValidationDSL<>(ruleValidation, elementValidation, name);
+    }
+
+    // adds the name to the Validation if present
+    private <R> Validation<R> validationAtName(Validation<R> refined) {
+        return name.fold(
+            () -> refined,
+            refined::at
+        );
     }
 }
--- dsl/src/main/java/be/iffy/fv/dsl/impl/VListValidationDSL.java
+++ dsl/src/main/java/be/iffy/fv/dsl/impl/VListValidationDSL.java
@@ -1,14 +1,15 @@
 package be.iffy.fv.dsl.impl;
 
 import be.iffy.fv.MappingRule;
-import be.iffy.fv.Rule;
 import be.iffy.fv.Validation;
 import be.iffy.fv.Validations;
 import io.vavr.collection.List;
+import io.vavr.control.Option;
 import org.jetbrains.annotations.Contract;
 
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * DSL class for validating a vavr List, allowing you to easily express rules on the list itself
@@ -17,31 +18,40 @@ import java.util.function.Function;
 public final class VListValidationDSL<L, E> {
     private final Validation<List<L>> listValidation;
     private final Validation<List<E>> elementValidation;
-    private final String name;
+    private final Option<String> name;
 
+    @SuppressWarnings("unchecked")
     public VListValidationDSL(List<L> value, String name) {
         this(
-                Rule.<List<L>>notNull().apply(value),
+                Validation.fromNullable(value),
                 //E and L start out the same
-                Rule.<List<E>>notNull().apply((List<E>) value),
-                name
+                Validation.fromNullable((List<E>) value),
+            Option.of(name)
         );
     }
 
-    private VListValidationDSL(Validation<List<L>> listValidation, Validation<List<E>> elementValidation, String name) {
+    private VListValidationDSL(Validation<List<L>> listValidation, Validation<List<E>> elementValidation, Option<String> name) {
         this.listValidation = Objects.requireNonNull(listValidation, "validation cannot be null");
         this.elementValidation = Objects.requireNonNull(elementValidation, "validation cannot be null");
-        this.name = name;
+        this.name = name.filter(Predicate.not(String::isBlank));
     }
 
+    /**
+     * Completes the validation process and returns the final {@link Validation} result.
+     * This combines both the list-level validations and the element-level validations.
+     */
     @Contract(pure = true)
     public Validation<List<E>> validate() {
         return Validations
-                .combine(listValidation.at(name), elementValidation.at(name)).map((list, elements) -> elements)
-                // make errors unique because something like a null list would appear in both validations
-                .mapErrors(List::distinct);
+                .combine(
+                    validationAtName(listValidation),
+                    validationAtName(elementValidation)
+                ).map((list, elements) -> elements);
     }
 
+    /**
+     * Applies the given validation rule to each element in the list.
+     */
     @Contract(pure = true)
     public <R> VListValidationDSL<R, R> eachIs(Function<E, Validation<R>> rule) {
         Validation<List<R>> newElements = elementValidation.refine(list -> MappingRule.of(rule).lift().toVavrList().apply(list));
@@ -56,12 +66,18 @@ public final class VListValidationDSL<L, E> {
     /**
      * Validates that the list satisfies the given rule.
      * This method is non-short-circuiting and will collect errors even if the list is already invalid.
-     *
-     * @param rule the rule for the list.
      */
     @Contract(pure = true)
     public VListValidationDSL<L, E> is(Function<List<L>, Validation<List<L>>> rule) {
         Validation<List<L>> ruleValidation = Validation.narrowSuper(listValidation.refine(MappingRule.of(rule)));
         return new VListValidationDSL<>(ruleValidation, elementValidation, name);
+    }
+
+    // adds the name to the Validation if present
+    private <R> Validation<R> validationAtName(Validation<R> refined) {
+        return name.fold(
+            () -> refined,
+            refined::at
+        );
     }
 }
