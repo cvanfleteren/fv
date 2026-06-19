caa106e16f0c66573afc61905e278538c1a99db5
Christophe Vanfleteren <c@vanfleteren.net>
2026-06-19 21:40:19 +0200

    javadoc + functionalInterface

--- core/src/main/java/be/iffy/fv/Transformation.java
+++ core/src/main/java/be/iffy/fv/Transformation.java
@@ -8,9 +8,11 @@ package be.iffy.fv;
  * Think of Transformation as a MappingRule on the same type that never fails, so there is
  * no need to return a Validation.
  * <p>
- * A good example of a Transformation is String.toUpperCase on a non-null String.
+ * A good example of a Transformation is String.toUpperCase on a non-null String, or most of the commons-lang StringUtils
+ * methods.
  * </p>
  */
+@FunctionalInterface
 public interface Transformation<T> {
 
     T apply(T value);
